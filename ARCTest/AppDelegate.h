f54a96673806dcb4fb78837a2fa86a033673059d
hua <hua@huatekiMacBook-Pro.local>
2014-04-05 16:29:19 +0800

    Initial Commit

---
+++ ARCTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ARCTest
+//
+//  Created by hua on 14-4-5.
+//  Copyright (c) 2014年 hua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ARCTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ARCTest
+//
+//  Created by hua on 14-4-5.
+//  Copyright (c) 2014年 hua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
